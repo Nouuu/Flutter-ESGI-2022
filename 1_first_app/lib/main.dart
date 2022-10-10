2f7295e515f57c9291e9a5b412f06d3d86ce0077
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-10-10 10:17:42 +0200

    Clear main

--- 1_first_app/lib/main.dart
+++ 1_first_app/lib/main.dart
@@ -1,17 +1,7 @@
-import 'package:first_app/user.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(Container(
     color: Colors.blue,
   ));
-  const User toto = User(
-    firstName: 'toto',
-    age: 12,
-  );
-  const User richard = User(
-    firstName: 'Richard',
-  );
-  print('${toto.firstName} ${toto.age}');
-  print('${richard.firstName} ${richard.age}');
 }
